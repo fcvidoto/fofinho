8af175a8d9db4fdd4c2ce2c8b8d29dc85a0c9b7e
Fernando Vidoto <fcvidoto@hotmail.com>
2016-11-17 11:23:26 -0200

    criadas as secoes de produtos e mapa

--- js/app.js
+++ js/app.js
@@ -9,4 +9,13 @@
 // 	largura = $('.content').css('width').replace('px', '');
 // });
 
-
+// // scroll no element (links da navbar)
+// var element_to_scroll_to = document.getElementById('anchorName2');
+// // Or:
+// var element_to_scroll_to = document.querySelectorAll('.my-element-class')[0];
+// // Or:
+// var element_to_scroll_to = $('.my-element-class')[0];
+// // Basically `element_to_scroll_to` just have to be a reference
+// // to any DOM element present on the page
+// // Then:
+// element_to_scroll_to.scrollIntoView();
