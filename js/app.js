7c3f08c61ab71cc9711006cf8c117918d193b09b
Fernando Vidoto <fcvidoto@hotmail.com>
2016-11-17 09:48:21 -0200

    fim do nav

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+// var largura;
+// $('.content').hover(function() {
+// 	$(this).css('width', largura * 2);
+// }, function() {
+// 	$(this).css('width', largura);
+// });
+// $(document).ready(function() {
+// 	largura = $('.content').css('width').replace('px', '');
+// });
+
+
