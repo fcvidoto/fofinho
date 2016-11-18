112255dcbd5d810d64fca52984d515fc7c0188c6
Fernando Vidoto <fcvidoto@hotmail.com>
2016-11-18 08:53:01 -0200

    css fix

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $("nav a").click(function() {
 		$('.navbar-collapse').collapse('hide'); // esconde o menu
 		var href = $(this).attr('href');
     $('html, body').animate({
-        scrollTop: $(href).offset().top - 80,
+        scrollTop: $(href).offset().top - 76,
         easing: 'swing',
     }, 700);
 });
